2116282cd30effea81fcae7b1cb99991d18102ee
Mihail <40296340+ykropchik@users.noreply.github.com>
2019-12-14 11:39:45 +1000

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -8,11 +8,11 @@
 TEST(isPalindrom, expectedResult){
     char* input = new char [100];
 
-    EXPECT_EQ(palindromeCheck(strcpy(input, "red rum sir is murder")), true);
-    EXPECT_EQ(palindromeCheck(strcpy(input, "false")), false);
-    EXPECT_EQ(palindromeCheck(strcpy(input, "top spot")), true);
-    EXPECT_EQ(palindromeCheck(strcpy(input, "hotel sdelat na russian, but don't work, because f*cking russian encoding in 2 bytes")), false);
-    EXPECT_EQ(palindromeCheck(strcpy(input, "and one more test")), false);
+    EXPECT_EQ(palindromeCheck((char*)"red rum sir is murder"), true);
+    EXPECT_EQ(palindromeCheck((char*)"false"), false);
+    EXPECT_EQ(palindromeCheck((char*)"top spot"), true);
+    EXPECT_EQ(palindromeCheck((char*)"hotel sdelat na russian, but don't work, because f*cking russian encoding in 2 bytes"), false);
+    EXPECT_EQ(palindromeCheck((char*)"and one more test"), false);
 }
 
 int main(int argc, char **argv){
